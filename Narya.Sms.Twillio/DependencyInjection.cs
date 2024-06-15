b78c812e74536e6b50c6344bd1421ed3d6a8b9dd
Hussein Dossoki <h.dossoki.ext@tahaluf.ae>
2024-06-15 17:04:03 +0300

    Add Twilio provider

---
+++ Narya.Sms.Twillio/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Narya.Sms.Core.Interfaces;
+using Narya.Sms.Core;
+using Narya.Sms.Twilio.Services;
+
+namespace Narya.Sms.Twilio;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddTwilioProvider(this IServiceCollection services)
+    {
+        services.AddSingleton<ISmsService, SmsService>();
+        services.AddSingleton<SmsService>();
+        var serviceProvider = services.BuildServiceProvider();
+        serviceProvider.AddSmsProvider("Twilio", serviceProvider.GetRequiredService<SmsService>());
+        return services;
+    }
+}
---
+++ Narya.Sms.Twillio/Services/SmsService.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Configuration;
+using Narya.Sms.Core.Extensions;
+using Narya.Sms.Core.Interfaces;
+using Narya.Sms.Core.Models;
+using Narya.Sms.Twilio.Extensions;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace Narya.Sms.Twilio.Services;
+
+public class SmsService : ISmsService
+{
+    private TwilioConfig _twilioConfig;
+
+    public SmsService(IConfiguration configuration)
+    {
+        _twilioConfig = configuration.GetTwilioConfig();
+    }
+
+    public async Task Send(SmsModel sms)
+    {
+        TwilioClient.Init(_twilioConfig.AccountSID, _twilioConfig.AuthToken);
+
+        var messageResource = await MessageResource.CreateAsync(
+            new PhoneNumber(sms.To),
+            from: new PhoneNumber(_twilioConfig.From),
+            body: sms.Message);
+    }
+
+    public async Task Send(SmsModel sms, dynamic configuration)
+    {
+        if (configuration is not object) throw new Exception("Twilio configuration is not a valid configurations.");
+        _twilioConfig = ModelExtension.ConvertTo<TwilioConfig>(configuration);
+        await Send(sms);
+    }
+}

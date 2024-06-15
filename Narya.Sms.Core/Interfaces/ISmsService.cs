f9c25290f3e4ad35a773a82ef87e2b29ef75b085
Hussein Dossoki <h.dossoki.ext@tahaluf.ae>
2024-06-15 17:22:58 +0300

    Add TestSuite WebApi project

--- Narya.Sms.Core/Interfaces/ISmsService.cs
+++ Narya.Sms.Core/Interfaces/ISmsService.cs
@@ -4,19 +4,6 @@ namespace Narya.Sms.Core.Interfaces;
 
 public interface ISmsService
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="sms"></param>
-    /// <returns></returns>
-    Task Send(SmsModel sms);
-
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="sms"></param>
-    /// <param name="configuration"></param>
-    /// <returns></returns>
-    Task Send(SmsModel sms, dynamic configuration);
+    Task Send(SmsModel options);
+    Task Send(SmsModel options, dynamic configuration);
 }
--- Narya.Sms.Twillio/Services/SmsService.cs
+++ Narya.Sms.Twillio/Services/SmsService.cs
@@ -1,37 +1,44 @@
 ﻿using Microsoft.Extensions.Configuration;
-using Narya.Sms.Core.Extensions;
-using Narya.Sms.Core.Interfaces;
 using Narya.Sms.Core.Models;
 using Narya.Sms.Twilio.Extensions;
+using Narya.Sms.Core.Interfaces;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
+using Narya.Sms.Core.Extensions;
 
 namespace Narya.Sms.Twilio.Services;
 
 public class SmsService : ISmsService
 {
-    private TwilioConfig _twilioConfig;
+    private readonly IConfiguration _configuration;
+    private TwilioConfig _twilioConfig = new();
 
     public SmsService(IConfiguration configuration)
     {
-        _twilioConfig = configuration.GetTwilioConfig();
+        _configuration = configuration;
     }
 
-    public async Task Send(SmsModel sms)
+    public async Task Send(SmsModel options)
+    {
+        _twilioConfig = _configuration.GetTwilioConfig();
+        await SendSms(options);
+    }
+
+    public async Task Send(SmsModel options, dynamic configuration)
+    {
+        if (configuration is not object) throw new Exception("Twilio configuration is not a valid configurations.");
+        _twilioConfig = ModelExtension.ConvertTo<TwilioConfig>(configuration);
+        await SendSms(options);
+    }
+
+    private async Task SendSms(SmsModel options)
     {
         TwilioClient.Init(_twilioConfig.AccountSID, _twilioConfig.AuthToken);
 
         var messageResource = await MessageResource.CreateAsync(
-            new PhoneNumber(sms.To),
+            new PhoneNumber(options.To),
             from: new PhoneNumber(_twilioConfig.From),
-            body: sms.Message);
-    }
-
-    public async Task Send(SmsModel sms, dynamic configuration)
-    {
-        if (configuration is not object) throw new Exception("Twilio configuration is not a valid configurations.");
-        _twilioConfig = ModelExtension.ConvertTo<TwilioConfig>(configuration);
-        await Send(sms);
+            body: options.Message);
     }
 }
